ba8e8537c8d9fee2445a12f65fda345829a98f6c
unknown <icto@ICTOP.uva.nl>
2014-06-19 16:33:44 +0200

    socket.io tutorial next step

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var app = require('express')();
+var express = require("express");
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = Number(process.env.PORT || 5000);
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
+
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
