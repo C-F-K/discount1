644d193a456235f070efe8818426a709517c1c3c
unknown <icto@ICTOP.uva.nl>
2014-06-19 16:38:05 +0200

    next tutorial step

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
@@ -11,7 +11,3 @@ var port = Number(process.env.PORT || 5000);
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-// app.listen(port, function() {
-//   console.log("Listening on " + port);
-// });
